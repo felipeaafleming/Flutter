9a8f22f55f14b37fc0e5d187fb525f9302474e4a
Felipe Fleming <felipeaafleming@gmail.com>
2020-04-19 19:51:03 -0300

    dice - complete dice project

--- Dice/lib/main.dart
+++ Dice/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  int random() {
+    return 1 + Random().nextInt(6);
+  }
+
+  void rollTheDice() {
+    setState(() {
+      leftDiceNumber = random();
+      rightDiceNumber = random();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rollTheDice();
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rollTheDice();
+              },
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
